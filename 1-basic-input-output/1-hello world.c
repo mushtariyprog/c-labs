3e02935be7b7d525bb28f4d7ff601bd6a81c64ba
mushtariyprog <mushtariyprog@gmail.com>
2021-11-09 17:16:31 +0300

    3ta fayl qo'shildi

---
+++ 1-basic-input-output/1-hello world.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main () {
+	char ism[10];
+	
+	printf("%s", ism);
+	scanf("%s", &ism);	
+	printf("Hello, world! %s", ism);
+	
+	
+	return 0;
+}
+	
+
---
+++ 1-basic-input-output/2-name.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+char name[10];
+char letter;
+int age;
+float kaloriya;
+
+printf("ismingizni kiriting:\n", name);
+scanf("%s",& name);
+
+printf("Yoqtirgan harfingizni kiriting:\n", letter);
+scanf("%s", &letter);
+
+printf("Yoshingizni kiriting:\n", age);
+scanf("%f", &age);
+
+printf("Kaloriyani kiriting:\n", age);
+scanf("%f", &kaloriya);
+
+
+	return 0;
+}
